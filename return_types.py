9bef06c1023521b42f6c0f6ad9a4e9c8892e4dae
Srijan Saurav <sauravsrijan@gmail.com>
2019-03-02 19:49:37 +0530

    Testing

---
+++ return_types.py
@@ -0,0 +1,23 @@
+def method1():
+    asdf = "qwert"
+    if True:
+        return None
+    return asdf
+
+def method2():
+    a_int = [1, 2, 3,]
+    b = (1, 2)
+    if len(a_int) == 2:
+        return a_int
+    else:
+        return b
+
+def no_return():
+    a='asdf'
+    b='asdff'
+
+def same_return_types():
+    if True:
+        return 5
+    else:
+        return 6
