ef73e169dc9aa87889c1ea8e58b96d5fdf1f8384
Srijan Saurav <sauravsrijan@gmail.com>
2019-03-02 16:25:26 +0530

    Checks open file checker

--- open_file.py
+++ open_file.py
@@ -1,6 +1,21 @@
-with open("open_file.py", r) as source:
-    pass
+import os
+import this
+import random
 
-f = open("open_file.py", r)
 
-f.close()
+def foo_bar_baz():
+    print("this should raise an issue")
+    print("jfsdof fjdsklf fjsdlkc ljvsldjvld vjsdlkcvmldfks jvlkdfsjvlkdfjvlkfdjv vjdflvjdflkvlfkdvlkdf vfjvlkdfjvlkdf vjdflkvjdf")
+    raise NotImplemented
+
+
+def open_new_file(filename):
+    """This will open a new file."""
+    file_desc = open(filename, 'r')
+    return file_desc.read()
+
+
+def open_new_file_ctx(filename):
+    """This will open a new file."""
+    with open(filename, 'r') as file_desc:
+        return file_desc.read()
