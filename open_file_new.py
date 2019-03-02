03d151a8794c279e1166b068251ddfc425678dc2
Srijan Saurav <sauravsrijan@gmail.com>
2019-03-02 19:16:44 +0530

    testing

--- open_file_new.py
+++ open_file_new.py
@@ -1,1 +1,10 @@
+import os
+import this
+
+def open_new_file_ctx(filename):
+    """This will open a new file."""
+    with open(filename, 'r') as file_desc:
+        return file_desc.read()
+
+
 a = open("data_class_test.py", 'r')
